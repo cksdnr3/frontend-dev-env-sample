889f98e98f8166fa60550157a596b36df0f09766
cksdnr3 <cksdnr3333@naver.com>
2021-08-17 21:03:18 +0900

    자주 사용하는 플러그인

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,9 @@
 const path = require("path");
+const webpack = require('webpack');
+const childProcess = require('child_process');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 module.exports = {
   mode: "development",
@@ -13,17 +18,47 @@ module.exports = {
     rules: [
       {
         test: /\.css$/,
-        use: ["style-loader", "css-loader"],
+        use: [
+            process.env.NODE_ENV === 'production'
+                ? MiniCssExtractPlugin.loader
+                : "style-loader",
+          "css-loader"
+        ],
       },
       {
         test: /\.(png|jpg|gif|svg)$/,
-        loader: 'url-loader',
-        options: {
-          publicPath: './dist/',
-          name: '[name].[ext]?[hash]',
-          limit: 20000, // 2kb
-        }
+        type: 'asset',
+
       }
     ],
   },
+  plugins: [
+      new webpack.BannerPlugin({
+        banner: `
+          Build Date: ${new Date().toLocaleString()}
+          Commit Version: ${childProcess.execSync('git rev-parse --short HEAD')}
+          Author: ${childProcess.execSync('git config user.name')}
+        `
+      }),
+      new webpack.DefinePlugin({
+        TWO: '1 + 1',
+        'api.domain': JSON.stringify('http://dev.api.domain.com')
+      }),
+      new HtmlWebpackPlugin({
+        template: "./src/index.html",
+        templateParameters: {
+          env: process.env.NODE_ENV === 'development' ? '(development)' : ''
+        },
+        minify: process.env.NODE_ENV === 'production' ? {
+          collapseWhitespace: true,
+          removeComments: true,
+        } : false
+      }),
+      new CleanWebpackPlugin({}),
+    ...(
+        process.env.NODE_ENV === 'production'
+            ? [new MiniCssExtractPlugin({ filename: '[name].css' })]
+            : []
+    )
+  ]
 };
