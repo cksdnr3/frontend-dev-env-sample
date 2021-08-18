b155bfbd0389bfb2b3f1f7ddd3500c7b15901be0
cksdnr3 <cksdnr3333@naver.com>
2021-08-18 22:36:13 +0900

    sample

--- src/app.js
+++ src/app.js
@@ -5,6 +5,6 @@ document.addEventListener('DOMContentLoaded' , () => {
     document.body.innerHTML = `<img src="${nyancat}" />`
 })
 
-console.log(process.env.NODE_ENV)
+console.log(process.env.NODE_ENV);
 console.log(TWO);
-console.log(api.domain)
+console.log(api.domain);
